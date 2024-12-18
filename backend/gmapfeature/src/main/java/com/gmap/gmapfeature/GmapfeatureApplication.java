c1a20571f3a2a31f346d8d2c317c2af0809d8a74
Nandhu Jayakumar <nandhujayakumar21@gmail.com>
2024-12-19 01:18:30 +0530

    Adding Backend Springboot basic dependencies and files

---
+++ backend/gmapfeature/src/main/java/com/gmap/gmapfeature/GmapfeatureApplication.java
@@ -0,0 +1,13 @@
+package com.gmap.gmapfeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmapfeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmapfeatureApplication.class, args);
+	}
+
+}
